f21f84ab2c14b12e543554b39715a6a815cf0b02
Luca Abbati <luca.abbati@datadoghq.com>
2020-11-02 11:37:21 +0100

    chaos: add some manual instrumentation to the chaos application

--- tests/chaos/app/public/index.php
+++ tests/chaos/app/public/index.php
@@ -1,7 +1,15 @@
 <?php
 
+use DDTrace\GlobalTracer;
+
+require __DIR__ . '/../vendor/autoload.php';
+
 require __DIR__ . '/chaos.php';
 $chaos = new Chaos($allowFatalAndUncaught = true);
 set_error_handler([$chaos, 'handleError']);
 set_exception_handler([$chaos, 'handleException']);
 $output = $chaos->randomRequestPath();
+
+$tracer = GlobalTracer::get();
+$scope = $tracer->startActiveSpan('my_dummy_operation');
+$scope->close();
